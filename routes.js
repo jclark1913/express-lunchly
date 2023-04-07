5748ea540892dc8bfdfcd61b66396bd23c364365
Terry Li <terryseanli@gmail.com>
2023-04-07 14:40:34 -0700

    added top 10 customer list and getTop10 Customer method

--- routes.js
+++ routes.js
@@ -13,7 +13,6 @@ const router = new express.Router();
 /** Homepage: show list of all customers or results of customer search. */
 
 router.get("/", async function (req, res, next) {
-  console.log("search", req.query.search);
   if(req.query.search === undefined) {
     const customers = await Customer.all();
     return res.render("customer_list.html", { customers });
@@ -22,6 +21,13 @@ router.get("/", async function (req, res, next) {
     const customers = await Customer.getByName(req.query.search);
     return res.render("customer_list.html", { customers });
   }
+});
+
+/** Show top 10 customers by number of reservations */
+
+router.get("/top-ten/", async function (req, res, next) {
+  const customers = await Customer.getTop10();
+  return res.render("customer_top-ten.html", { customers });
 });
 
 /** Form to add a new customer. */
